cbe9371246f9c062de5efb001cf22e4c7446879e
Nick Sayer <nsayer@users.noreply.github.com>
2015-04-23 09:34:47 -0700

    Release 1.0
    1. Move strings to PROGMEM
    2. Add a UI button - allows changing display modes
    3. Add a min/max reading voltmeter for the pilot line. Use the button to toggle between
    showing the min and max voltage on the pilot line and showing the J1772 ampacity.
    
    Note that items 2 and 3 require hardware version 4.x, though it's backwards compatible down to hardware v3.3.

--- EV_Sim_84.ino
+++ EV_Sim_84.ino
@@ -20,7 +20,7 @@
 
 #include <Arduino.h>
 
-#define VERSION "(84) 0.2"
+#define VERSION "(84) 1.0"
 
 // The pins connected up to the LCD.
 #define LCD_D4 7
@@ -32,11 +32,39 @@
 
 #define PILOT_DIGITAL_SAMPLING_PIN  0
 
-#define SAMPLE_PERIOD 500
+#define PILOT_ANALOG_SAMPLING_PIN 2
+
+#define BUTTON_PIN 8
+
+#define SAMPLE_PERIOD 350
+
+// The button selects from different available display modes.
+// At the moment, there are two: J1772 ampacity and min/max voltage
+#define MODE_COUNT 2
+
+// The different types of button events.
+#define BUTTON_NONE 0
+#define BUTTON_SHORT 1
+#define BUTTON_LONG 2
+
+// When the button state changes, further state changes are ignored for this many ms.
+#define DEBOUNCE_INTERVAL 50
+// The boundary between a BUTTON_SHORT and a BUTTON_LONG in ms.
+#define LONG_PRESS_INTERVAL 250
+
+// The scale and offset for the linear formula that relates A/D reading to pilot voltage.
+// A circuit simulation shows the readback voltage as 4.543 at 12 volts in and 891 mV at -12 volts.
+// Since a single A/D unit represents 4.88 mV, we can work out a formula to convert A/D readings
+// into millivolts of the actual pilot signal
+#define PILOT_READ_SCALE 32
+#define PILOT_READ_OFFSET (-556)
 
 #include <LiquidCrystal.h>
 
-// Turn on auto-detect
+// Keep strings in PROGMEM. Copy them into this temp buffer just-in-time.
+char p_buffer[96];
+#define P(str) (strcpy_P(p_buffer, PSTR(str)), p_buffer)
+
 LiquidCrystal display(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 // special return result for the 'digital communications' pilot.
@@ -75,27 +103,72 @@ static inline unsigned long dutyToMA(unsigned long duty) {
   }
 }
 
+static inline char read_button() {
+  static unsigned char last_state = HIGH; // HIGH is unpressed, LOW is pressed
+  static unsigned long debounce_time = 0; // ignore events until this time
+  static unsigned long button_press_start = 0;
+  if (debounce_time != 0) {
+    if (millis() > debounce_time) {
+      debounce_time = 0; // debounce period is over
+    } else {
+      return BUTTON_NONE;
+    }
+  }
+  unsigned char state = digitalRead(BUTTON_PIN);
+  if (state == last_state) return BUTTON_NONE;
+  last_state = state;
+  debounce_time = millis() + DEBOUNCE_INTERVAL;
+  switch(state) {
+    case LOW:
+      button_press_start = millis();
+      return BUTTON_NONE;
+      break;
+    case HIGH:
+      unsigned char out = (millis() - button_press_start > LONG_PRESS_INTERVAL)?BUTTON_LONG:BUTTON_SHORT;
+      button_press_start = 0;
+      return out;
+      break;
+  }
+}
+
+static inline int scale_mv(unsigned int value) {
+  return (((int)value) + PILOT_READ_OFFSET) * PILOT_READ_SCALE;
+}
+
 void setup() {
   
   pinMode(PILOT_DIGITAL_SAMPLING_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(PILOT_ANALOG_SAMPLING_PIN, INPUT);
+  analogReference(DEFAULT);
     
   display.begin(16, 2);
   
   display.clear();
   
-  display.print("EV Sim ");
-  display.print(VERSION);
+  display.print(P("EV Sim "));
+  display.print(P(VERSION));
   
   delay(2000);
   display.clear();
 }
 
 void loop() {
-
+  static unsigned char mode = 0;
   unsigned int last_state = 99; // neither HIGH nor LOW
   unsigned long high_count = 0, low_count = 0, state_changes = -1; // ignore the first change from "invalid"
+  unsigned int high_analog=0, low_analog=0xffff;
   
   for(unsigned long start_poll = millis(); millis() - start_poll < SAMPLE_PERIOD; ) {
+    switch(read_button()) {
+      case BUTTON_NONE:
+        break;
+      case BUTTON_SHORT:
+      case BUTTON_LONG:
+        if (++mode >= MODE_COUNT) mode = 0;
+        break;
+    }
+    
     unsigned int state = digitalRead(PILOT_DIGITAL_SAMPLING_PIN);
     
     if (state == LOW)
@@ -107,28 +180,42 @@ void loop() {
       state_changes++;
       last_state = state;
     }
+
+    unsigned int analog = analogRead(PILOT_ANALOG_SAMPLING_PIN);
+    if (analog > high_analog) high_analog = analog;
+    if (analog < low_analog) low_analog = analog;
   }
+  
   char buf[32];
   unsigned long amps = 0;
   if (state_changes == 0) {
-    sprintf(buf, "   0 Hz     %s   ", (low_count>high_count)?"-":"+");
+    sprintf(buf, P("   0 Hz     %s   "), (low_count>high_count)?"-":"+");
   } else {
     unsigned int duty = (high_count * 1000) / (high_count + low_count);
     duty %= 1000; // turn 100% into 0% just for display purposes. A 100% duty cycle doesn't really make sense.
   
-    unsigned long frequency = (state_changes / 2) * (1000 / SAMPLE_PERIOD);
+    unsigned long frequency = ((state_changes / 2) * 1000) / SAMPLE_PERIOD;
 
     amps = dutyToMA(duty);
 
-    sprintf(buf, "%4ld Hz   %2d.%01d %%", frequency, duty / 10, duty % 10);
+    sprintf(buf, P("%4ld Hz   %2d.%01d %%"), frequency, duty / 10, duty % 10);
   }
   display.setCursor(0, 0);
   display.print(buf);
 
-  if (amps == DIGITAL_COMM_REQD) {
-    sprintf(buf, "Digital");
-  } else {
-    sprintf(buf, "%2ld.%02ld A", amps / 1000, (amps % 1000) / 10);
+  switch(mode) {
+    case 0:
+      if (amps == DIGITAL_COMM_REQD) {
+        sprintf(buf, P("Digital         "));
+      } else {
+        sprintf(buf, P("%2ld.%02ld A         "), amps / 1000, (amps % 1000) / 10);
+      }
+      break;
+    case 1:
+      int low_mv = scale_mv(low_analog);
+      int high_mv = scale_mv(high_analog);
+      sprintf(buf,P(" %+03d.%01d  %+03d.%01d"), low_mv/1000, abs(low_mv % 1000) / 100, high_mv/1000, abs(high_mv % 1000) / 100);
+      break;
   }
   
   display.setCursor(0, 1);
